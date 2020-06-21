cc77de02715e934b16f258ac9a98b8641cab6d7a
ricson.sampaio <ricson.sampaio>
2020-06-21 17:08:12 -0300

    Adding Dashboard

--- control-panel-machines/src/App.js
+++ control-panel-machines/src/App.js
@@ -1,51 +1,230 @@
-import React, { useState } from "react";
-import Terminal from "terminal-in-react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+
+import Icon from "@material-ui/core/Icon";
+import Store from "@material-ui/icons/Store";
+import Warning from "@material-ui/icons/Warning";
+import DateRange from "@material-ui/icons/DateRange";
+import LocalOffer from "@material-ui/icons/LocalOffer";
+import Update from "@material-ui/icons/Update";
+import Accessibility from "@material-ui/icons/Accessibility";
+import BugReport from "@material-ui/icons/BugReport";
+import Code from "@material-ui/icons/Code";
+import Cloud from "@material-ui/icons/Cloud";
+
+
+import Table from "components/Table/Table.js";
+import CustomTabs from "components/CustomTabs/CustomTabs.js";
+import Tasks from "components/Tasks/Tasks.js";
+import GridItem from "components/Grid/GridItem.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import Card from "components/Card/Card.js";
+import CardHeader from "components/Card/CardHeader.js";
+import CardIcon from "components/Card/CardIcon.js";
+import CardBody from "components/Card/CardBody.js";
+import CardFooter from "components/Card/CardFooter.js";
+import Danger from "components/Typography/Danger.js";
+
+import { bugs, website, server } from "variables/general.js";
 
 function App() {
-  const [captura, setCaptura] = useState("");
-
-  const showMsg = () => "Hello World";
-
   return (
-    <>
-      <div>Comando digitado - {captura}</div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-        }}
-      >
-        <Terminal
-          color="green"
-          backgroundColor="black"
-          barColor="black"
-          style={{ fontWeight: "bold", fontSize: "1em" }}
-          commands={{
-            showmsg: () => {
-              setCaptura("showmsg");
-              showMsg();
-            },
-            popup: () => {
-              setCaptura("popup");
-              alert("Terminal in React");
-            },
-            test: () => {
-              setCaptura("test");
-            },
-          }}
-          descriptions={{
-            showmsg: "shows a message",
-            alert: "alert",
-            popup: "alert",
-            test: "Comando de teste",
-          }}
-          msg="You can write anything here. Example - Hello! My name is Foo and I like Bar."
-        />
-      </div>
-    </>
+    <div>
+      <GridContainer>
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="warning" stats icon>
+              <CardIcon color="warning">
+                <Icon>content_copy</Icon>
+              </CardIcon>
+              <p className={"cardCategory"}>Used Space</p>
+              <h3 className={"cardTitle"}>
+                49/50 <small>GB</small>
+              </h3>
+            </CardHeader>
+            <CardFooter stats>
+              <div className={"stats"}>
+                <Danger>
+                  <Warning />
+                </Danger>
+                <a href="#pablo" onClick={e => e.preventDefault()}>
+                  Get more space
+                </a>
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="success" stats icon>
+              <CardIcon color="success">
+                <Store />
+              </CardIcon>
+              <p className={"cardCategory"}>Revenue</p>
+              <h3 className={"cardTitle"}>$34,245</h3>
+            </CardHeader>
+            <CardFooter stats>
+              <div className={"stats"}>
+                <DateRange />
+                Last 24 Hours
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="danger" stats icon>
+              <CardIcon color="danger">
+                <Icon>info_outline</Icon>
+              </CardIcon>
+              <p className={"cardCategory"}>Fixed Issues</p>
+              <h3 className={"cardTitle"}>75</h3>
+            </CardHeader>
+            <CardFooter stats>
+              <div className={"stats"}>
+                <LocalOffer />
+                Tracked from Github
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="info" stats icon>
+              <CardIcon color="info">
+                <Accessibility />
+              </CardIcon>
+              <p className={"cardCategory"}>Followers</p>
+              <h3 className={"cardTitle"}>+245</h3>
+            </CardHeader>
+            <CardFooter stats>
+              <div className={"stats"}>
+                <Update />
+                Just Updated
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+      </GridContainer>
+      <GridContainer>
+        <GridItem xs={12} sm={12} md={4}>
+          <Card>
+            <CardHeader color="info" stats icon>
+              <CardIcon color="info">
+                <Accessibility />
+              </CardIcon>
+              <p className={"cardCategory"}>Followers</p>
+              <h3 className={"cardTitle"}>+245</h3>
+            </CardHeader>
+            <CardFooter stats>
+              <div className={"stats"}>
+                <Update />
+                Just Updated
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={12} md={4}>
+          <Card>
+            <CardHeader color="info" stats icon>
+              <CardIcon color="info">
+                <Accessibility />
+              </CardIcon>
+              <p className={"cardCategory"}>Followers</p>
+              <h3 className={"cardTitle"}>+245</h3>
+            </CardHeader>
+            <CardFooter stats>
+              <div className={"stats"}>
+                <Update />
+                Just Updated
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={12} md={4}>
+          <Card>
+            <CardHeader color="info" stats icon>
+              <CardIcon color="info">
+                <Accessibility />
+              </CardIcon>
+              <p className={"cardCategory"}>Followers</p>
+              <h3 className={"cardTitle"}>+245</h3>
+            </CardHeader>
+            <CardFooter stats>
+              <div className={"stats"}>
+                <Update />
+                Just Updated
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+      </GridContainer>
+      <GridContainer>
+        <GridItem xs={12} sm={12} md={6}>
+          <CustomTabs
+            title="Tasks:"
+            headerColor="primary"
+            tabs={[
+              {
+                tabName: "Bugs",
+                tabIcon: BugReport,
+                tabContent: (
+                  <Tasks
+                    checkedIndexes={[0, 3]}
+                    tasksIndexes={[0, 1, 2, 3]}
+                    tasks={bugs}
+                  />
+                )
+              },
+              {
+                tabName: "Website",
+                tabIcon: Code,
+                tabContent: (
+                  <Tasks
+                    checkedIndexes={[0]}
+                    tasksIndexes={[0, 1]}
+                    tasks={website}
+                  />
+                )
+              },
+              {
+                tabName: "Server",
+                tabIcon: Cloud,
+                tabContent: (
+                  <Tasks
+                    checkedIndexes={[1]}
+                    tasksIndexes={[0, 1, 2]}
+                    tasks={server}
+                  />
+                )
+              }
+            ]}
+          />
+        </GridItem>
+        <GridItem xs={12} sm={12} md={6}>
+          <Card>
+            <CardHeader color="warning">
+              <h4 className={"cardTitleWhite"}>Employees Stats</h4>
+              <p className={"cardCategoryWhite"}>
+                New employees on 15th September, 2016
+              </p>
+            </CardHeader>
+            <CardBody>
+              <Table
+                tableHeaderColor="warning"
+                tableHead={["ID", "Name", "Salary", "Country"]}
+                tableData={[
+                  ["1", "Dakota Rice", "$36,738", "Niger"],
+                  ["2", "Minerva Hooper", "$23,789", "Curaçao"],
+                  ["3", "Sage Rodriguez", "$56,142", "Netherlands"],
+                  ["4", "Philip Chaney", "$38,735", "Korea, South"]
+                ]}
+              />
+            </CardBody>
+          </Card>
+        </GridItem>
+      </GridContainer>
+    </div>
   );
 }
 
