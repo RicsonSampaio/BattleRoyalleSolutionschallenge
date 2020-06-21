c05dc053f33399dad1a828507317df799f69f01e
ricson.sampaio <ricson.sampaio>
2020-06-21 14:23:47 -0300

    testando terminal component

--- control-panel-machines/src/App.js
+++ control-panel-machines/src/App.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Terminal from "terminal-in-react";
+import "./App.css";
 
 function App() {
+  const [captura, setCaptura] = useState("");
+
+  const showMsg = () => "Hello World";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>Comando digitado - {captura}</div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <Terminal
+          color="green"
+          backgroundColor="black"
+          barColor="black"
+          style={{ fontWeight: "bold", fontSize: "1em" }}
+          commands={{
+            showmsg: () => {
+              setCaptura("showmsg");
+              showMsg();
+            },
+            popup: () => {
+              setCaptura("popup");
+              alert("Terminal in React");
+            },
+            test: () => {
+              setCaptura("test");
+            },
+          }}
+          descriptions={{
+            showmsg: "shows a message",
+            alert: "alert",
+            popup: "alert",
+            test: "Comando de teste",
+          }}
+          msg="You can write anything here. Example - Hello! My name is Foo and I like Bar."
+        />
+      </div>
+    </>
   );
 }
 
