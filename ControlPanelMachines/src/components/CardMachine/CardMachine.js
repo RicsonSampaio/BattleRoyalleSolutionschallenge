26825e5e1c512ca73db2be4912d46961474f47c7
ricson.sampaio <ricson.sampaio>
2020-06-23 01:15:43 -0300

    web cmd pushing and receiving from backend

--- ControlPanelMachines/src/components/CardMachine/CardMachine.js
+++ ControlPanelMachines/src/components/CardMachine/CardMachine.js
@@ -1,9 +1,9 @@
-import React from "react";
-
+import React,{useState} from "react";
+import axios from 'axios';
 import { makeStyles } from "@material-ui/core/styles";
 import Icon from "@material-ui/core/Icon";
-import Warning from "@material-ui/icons/Warning";
-import Danger from "components/Typography/Danger.js";
+import Terminal from "terminal-in-react";
+import Switch from '@material-ui/core/Switch';
 
 import GridItem from "components/Grid/GridItem.js";
 import Card from "components/Card/Card.js";
@@ -14,11 +14,19 @@ import CardFooter from "components/Card/CardFooter.js";
 import styles from "assets/jss/material-dashboard-react/views/dashboardStyle.js";
 const useStyles = makeStyles(styles);
 
+// const [switchC, setSwitchC] = useState({
+//     checkedA: true,
+//     checkedB: true,
+//   });
+
+const handleChange = (event) => {
+    //setSwitchC({ ...switchC, [event.target.name]: event.target.checked });
+};
+
 const CardMachine = ({
   nome,
   ip,
   antiVirusName,
-  hasAntiVirus,
   hds,
   firewall,
   winVersion,
@@ -26,44 +34,95 @@ const CardMachine = ({
 }) => {
   const classes = useStyles();
   return (
-    <GridItem xs={12} sm={6} md={4}>
-      <Card>
-        <CardHeader color="warning" stats icon>
-          <CardIcon color="warning">
-            <Icon>content_copy</Icon>
-          </CardIcon>
-          <p className={classes.cardCategory}>{nome}</p>
-          <h3 className={classes.cardTitle}>
-            <small>IP:</small> {ip}
-          </h3>
-          <h3 className={classes.cardTitle}>
-            <small>Anti Virus:</small>{hasAntiVirus} {antiVirusName}
-          </h3>
-          <h3 className={classes.cardTitle}>
-            <small>HDs:</small> {hds} 
-          </h3>
-          <h3 className={classes.cardTitle}>
-            <small>Firewall:</small> {firewall} 
-          </h3>
-          <h3 className={classes.cardTitle}>
-            <small>Versão do Windows:</small> {winVersion} 
-          </h3>
-          <h3 className={classes.cardTitle}>
-            <small>Versão do .Net:</small> {dotNetVersion}
-          </h3>
-        </CardHeader>
-        <CardFooter stats>
-          <div className={classes.stats}>
-            <Danger>
-              <Warning />
-            </Danger>
-            <a href="" onClick={(e) => e.preventDefault()}>
-              Button toggle Machine state
-            </a>
-          </div>
-        </CardFooter>
-      </Card>
+      <>
+        <GridItem xs={12} sm={6} md={6}>
+            <Card> 
+                <CardHeader color="primary" stats icon>
+                <CardIcon color="primary">
+                    <Icon>computer</Icon>
+                </CardIcon>
+                <p className={classes.cardCategory}>{nome}</p>
+                <h3 className={classes.cardTitle}>
+                    <small>IP:</small> <br />
+                    {ip}
+                </h3>
+                <h3 className={classes.cardTitle}>
+                    <small>Anti Virus:</small><br />
+                    {antiVirusName}
+                </h3>
+                <h3 className={classes.cardTitle}>
+                    <small>HDs:</small> <br />
+                    {hds} 
+                </h3>
+                <h3 className={classes.cardTitle}>
+                    <small>Firewall:</small> <br />
+                    {firewall} 
+                </h3>
+                <h3 className={classes.cardTitle}>
+                    <small>Versão do Windows:</small> <br />
+                    {winVersion} 
+                </h3>
+                <h3 className={classes.cardTitle}>
+                    <small>Versão do .Net:</small> <br />
+                    {dotNetVersion}
+                </h3>
+                </CardHeader>
+                <div>
+                    <h3 className={classes.cardTitle}>
+                        <small>Turn on/off machine</small> 
+                    </h3>
+                    <Switch
+                        checked={true}
+                        onChange={handleChange}
+                        color="primary"
+                        name="checkedB"
+                        inputProps={{ 'aria-label': 'primary checkbox' }}
+                    />
+                </div>        
+            </Card>
+        </GridItem>
+        <GridItem xs={12} sm={6} md={6}>
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "100vh",
+                }}
+            >
+            <Terminal
+                color="green"
+                backgroundColor="black"
+                barColor="black"
+                style={{ fontWeight: "bold", fontSize: "1em" }}
+                commands={{
+                run: (args, print, runCommand) => {
+                    //setCaptura(args);
+                    let cmdCommand = '';
+                    // cmdCommand = args.forEach((element, index, array) => {
+                    //   if(index != 0){
+                    //     cmdCommand +=' ' + element
+                    //   }//index != 0 ? cmdCommand +=' ' + element : null
+                    // });//args.toString().substring(3,args.length)
+
+                    for (let index = 0; index < args.length; index++) {
+                    if(index != 0){
+                        cmdCommand +=' ' + args[index]
+                    }
+                    }
+                    
+                    axios.post(`http://localhost:5000/command-cmd?CMDCommand=${cmdCommand}&MachineId=1`)
+                    .then(res => {
+                    const commandResult = res.data;
+                    print(commandResult);
+                    })
+                }
+                }}
+                msg="To run a command: 'run' + cmdcommand. Example: run ipconfig | run dir | run ping www.google.com | "
+            />
+        </div>
     </GridItem>
+  </>
   );
 };
 
--- ControlPanelMachines/src/views/Dashboard/Dashboard.js
+++ ControlPanelMachines/src/views/Dashboard/Dashboard.js
@@ -2,14 +2,13 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import GridContainer from "components/Grid/GridContainer.js";
 import CardMachine from "components/CardMachine/CardMachine.js";
-import Terminal from "terminal-in-react";
 
 
 export default function Dashboard() {
 
   const [machines, setMachines] = useState([]);
-  const [captura, setCaptura] = useState("");
-  const showMsg = () => "Hello World";
+  
+  //const [captura, setCaptura] = useState("");
 
   useEffect(() => {
     axios.get(`http://localhost:5000/monitoring-machines/getAll`)
@@ -20,41 +19,8 @@ export default function Dashboard() {
         })
   }, [setMachines]);
 
-  // state = {
-  //   name: '',
-  // }
-
-  // handleSubmit = event => {
-  //   event.preventDefault();
-
-  //   const user = {
-  //     name: this.state.name
-  //   };
-
-  //   axios.post(`https://jsonplaceholder.typicode.com/users`, { user })
-  //     .then(res => {
-  //       console.log(res);
-  //       console.log(res.data);
-  //     })
-  // }
-
-  // handleChange = event => {
-  //   this.setState({ name: event.target.value });
-  // }
-
   return (
     <>
-
-      {/* <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Person Name:
-            <input type="text" name="name" onChange={this.handleChange} />
-          </label>
-          <button type="submit">Add</button>
-        </form>
-      </div> */}
-      
       <GridContainer>
       {machines.map(machine => {
         return <CardMachine
@@ -62,53 +28,13 @@ export default function Dashboard() {
                 nome={machine.name}
                 ip={machine.address}
                 antiVirusName={machine.antivirus.name}
-                hasAntiVirus={machine.antivirus.hasAntivirus}
-                hds={machine.hardDrive.map(hd => (` [Nome: ${hd.name}, | FreeSpace: ${hd.freeSpace}, | TotalSpace: ${hd.usedSpace} ] `))}
-                firewall={machine.antivirus.hasAntivirus}
+                hds={machine.hardDrive.map(hd => (` [Nome: ${hd.name} | FreeSpace: ${hd.freeSpace}, | TotalSpace: ${hd.usedSpace} ] `))}
+                firewall={String(machine.antivirus.hasAntivirus)}
                 winVersion={machine.osVersion}
                 dotNetVersion={machine.dotNetVersion}
               />
       })}
         
-      </GridContainer>
-
-      <GridContainer>
-      <div>Comando digitado - {captura}</div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-        }}
-      >
-        <Terminal
-          color="green"
-          backgroundColor="black"
-          barColor="black"
-          style={{ fontWeight: "bold", fontSize: "1em" }}
-          commands={{
-            showmsg: () => {
-              setCaptura("showmsg");
-              showMsg();
-            },
-            popup: () => {
-              setCaptura("popup");
-              alert("Terminal in React");
-            },
-            test: () => {
-              setCaptura("test");
-            },
-          }}
-          descriptions={{
-            showmsg: "shows a message",
-            alert: "alert",
-            popup: "alert",
-            test: "Comando de teste",
-          }}
-          msg="You can write anything here. Example - Hello! My name is Foo and I like Bar."
-        />
-      </div>
       </GridContainer>
     </>
   );
